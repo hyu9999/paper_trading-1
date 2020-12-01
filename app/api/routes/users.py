b706b796f2d30ba55f91dcba1a4f2815939677c8
Caoyiii <chaunceywe@gmail.com>
2020-12-01 12:27:24 +0800

    refactor: 修改用户总资产显示的值(包括被冻结金额)

--- app/api/routes/users.py
+++ app/api/routes/users.py
@@ -2,9 +2,11 @@ from starlette import status
 from fastapi import APIRouter, Depends, Path
 
 from app.api.dependencies.database import get_repository
+from app.db.repositories.order import OrderRepository
 from app.db.repositories.user import UserRepository
 from app.exceptions.db import EntityDoesNotExist
 from app.exceptions.http import InvalidUserID
+from app.models.enums import OrderStatusEnum
 from app.models.types import PyObjectId
 from app.models.schemas.users import ListOfUserInResponse, UserInResponse
 
@@ -20,9 +22,14 @@ router = APIRouter()
 async def get_user(
     user_id: PyObjectId = Path(..., description="用户ID"),
     user_repo: UserRepository = Depends(get_repository(UserRepository)),
+    order_repo: OrderRepository = Depends(get_repository(OrderRepository)),
 ) -> UserInResponse:
     try:
         user = await user_repo.get_user_by_id(user_id)
+        orders = await order_repo.get_orders(user_id=user_id, status=[OrderStatusEnum.NOT_DONE,
+                                                                      OrderStatusEnum.SUBMITTING])
+        user.assets = user.assets.to_decimal() + \
+            sum(filter(None, [order.frozen_amount.to_decimal() for order in orders if order.frozen_amount]))
     except EntityDoesNotExist:
         raise InvalidUserID
     return UserInResponse(**user.dict())
